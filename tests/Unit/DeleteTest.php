c309b615c51848b26303bae522ff187c71139a17
devanshisri <ds846@njit.edu>
2018-03-29 22:52:45 -0400

    Unit test for Delete user in user table done.

---
+++ tests/Unit/DeleteTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class DeleteTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testDeleteUserUnit()
+    {
+        $user = new User();
+        $user->name = 'hermoine';
+        $user->email = 'hermoine@gmail.com';
+        $user->password = 'harrypotter';
+        $user->save();
+        $this->assertTrue($user->delete());
+        $this->assertTrue(true);
+    }
+}
