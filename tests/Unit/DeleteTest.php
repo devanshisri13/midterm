59806fde783874ab5ed551b3196a5d508ad8f4bc
devanshisri <ds846@njit.edu>
2018-03-29 23:14:03 -0400

    Unit test for deleting a car in cars table done.

--- tests/Unit/DeleteTest.php
+++ tests/Unit/DeleteTest.php
@@ -6,6 +6,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\User;
+use App\Car;
 
 class DeleteTest extends TestCase
 {
@@ -23,5 +24,14 @@ class DeleteTest extends TestCase
         $user->save();
         $this->assertTrue($user->delete());
         $this->assertTrue(true);
+    }
+        public function testDeleteCarUnit()
+    {
+        $car = new car();
+        $car->Make = 'honda';
+        $car->Model = 'b';
+        $car->year = '1969';
+        $car->save();
+        $this->assertTrue($car->delete());
     }
 }
